011f515eea6746fdad04ba079dd5ee0310e7c567
rinarai4013-debug <rinarai4013-debug@users.noreply.github.com>
2026-04-08 16:01:38 -0700

    Remove ignoreBuildErrors from Next config

--- next.config.mjs
+++ next.config.mjs
@@ -3,9 +3,6 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
   images: {
     formats: ["image/avif", "image/webp"],
   },
